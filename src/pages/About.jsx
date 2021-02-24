b7cd183897b7cd66652a97668a7c2422517e7e7f
vbliakhar <vovabliahar@gmail.com>
2021-02-24 12:02:14 +0100

    Adder Router

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">Informations </h1>
+        <p className="lead">
+          This is a modified jumbotron that occupies the entire ...
+          <strong>1.0.0</strong>
+        </p>
+      </div>
+    </div>
+  );
+};
