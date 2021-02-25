6e5d0713ad742c3ff78bd392bf399fe85d31c247
vbliakhar <vovabliahar@gmail.com>
2021-02-25 11:40:47 +0100

    created a page Home(card)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import { Card } from "../components/Card";
+import { Search } from "../components/Search";
 export const Home = () => {
+  const cards = new Array(6).fill("");
+  console.log(cards);
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Fragment>
+      <h1>Home</h1>
+      <Search />
+      <div className="row">
+        {cards.map((_, card) => {
+          return (
+            <div className="col-sm-4 mb-4" key={card}>
+              <Card />
+            </div>
+          );
+        })}
+      </div>
+    </Fragment>
   );
 };
